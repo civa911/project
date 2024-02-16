ee51a20d51abfc55d963a8c968ed3c117900e60c
Vlad Tsiv <vlad_tsiv82@mail.ru>
2024-02-16 15:53:49 +0300

    inittial commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat-css');
+const plumber = require('gulp-plumber');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+function html() {
+  return gulp.src('src/**/*.html')
+        .pipe(plumber())
+        .pipe(gulp.dest('dist/'))
+}
+
+exports.html = html;
+
+function css() {
+  return gulp.src('src/blocks/**/*.css')
+        .pipe(plumber())
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest('dist/'))
+}
+
+exports.css = css;
